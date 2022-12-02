94320e7f2488d622b440b92d7e0848edb5353629
johnjahbless <johnjahbless@gmail.com>
2022-12-02 11:45:37 +0100

    controlled-uncontrolled2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import { UncontrolledForm } from './UncontrolledForm';
 import { ControlledForm } from './ControlledForm';
+import { UncontrolledModal } from './UncontrolledModal';
+import { ControlledModal} from './ControlledModel';
+import { useState } from 'react';
+import { UnControlledOnboardingFlow } from './UnControlledOnboardingFlow';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
+
+const StepOne = ({goToNext}) => {
+	return<>
+<h1>Step 1</h1>
+<button onClick={()=> goToNext({name: "john"})}>Next</button>
+</>
+}
+const StepTwo = ({goToNext}) => {
+	return<>
+<h1>Step 2</h1>
+<button onClick={()=>goToNext({age: 100})}>Next</button>
+</>
+}
+const StepThree = ({goToNext}) => {
+	return<>
+<h1>Step 3</h1>
+<p>Congratulations, you are qualified for senior discount</p>
+<button onClick={()=>goToNext({})}>Next</button>
+</>
+}
+const StepFour = ({goToNext}) => {
+	return<>
+<h1>Step 4</h1>
+<button onClick={()=>goToNext({hairColor: 'brown'})}>Next</button>
+</>
+}
+
 
 function App() {
+	const [onboardingData, setOnboardingData] = useState({});
+    const [currentIndex, setCurrentIndex] = useState(0);
+	const onNext = stepData => {
+		setOnboardingData({onboardingData, stepData});
+		setCurrentIndex(currentIndex + 1)
+    }
+
 	return (
-		<ControlledForm />
+		<>
+		<ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext}>
+			<StepOne/>
+			<StepTwo/>
+			{onboardingData.age >= 62 && <StepThree/> }
+			<StepFour />
+		</ControlledOnboardingFlow>
+		{/* <ControlledModal shouldShow={shouldShowModal} onRequestShow={() => setShouldShowModal(false)}>
+			<h1>jjhgghfgfhfftyyftyf</h1>
+			</ControlledModal>
+		<button onClick={() => setShouldShowModal(!shouldShowModal)}>{shouldShowModal ? "Hide modal" : "Show Modal"}</button> */}
+		</>
 	);
 }
 
